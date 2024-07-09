978bcc1a38f969198ab1193b458974f3e7ea6a9f
Keijur0 <tran.qlam@gmail.com>
2024-07-09 18:40:40 +0200

    Front E2E Alltests + refactor

---
+++ front/cypress/e2e/alltests.cy.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+import LoginSpec from "./login.cy";
+import MeSpec from "./me.cy";
+import RegisterSpec from "./register.cy";
+import SessionDetailSpec from "./sessiondetail.cy";
+import SessionFormSpec from "./sessionform.cy";
+import SessionListSpec from "./sessionlist.cy";
+
+
+LoginSpec();
+MeSpec();
+RegisterSpec();
+SessionDetailSpec();
+SessionFormSpec();
+SessionListSpec();
--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,64 +1,66 @@
 /// <reference types="cypress" />
 
-describe('Login Component', () => {
+export default function LoginSpec() {  
+  describe('Login Component', () => {
 
-  beforeEach(() => {
-    cy.visit('/login');
-  });
+    beforeEach(() => {
+      cy.visit('/login');
+    });
 
-  it('should display login form', () => {
-    cy.get('input[formControlName=email]').should('be.visible');
-    cy.get('input[formControlName=password]').should('be.visible');
-  });
+    it('should display login form', () => {
+      cy.get('input[formControlName=email]').should('be.visible');
+      cy.get('input[formControlName=password]').should('be.visible');
+    });
 
-  it('should display an error message on failed login', () => {
-    cy.intercept('POST', '/api/auth/login', {
-      statusCode: 401,
-      body: { message: 'Unauthorized' }
+    it('should display an error message on failed login', () => {
+      cy.intercept('POST', '/api/auth/login', {
+        statusCode: 401,
+        body: { message: 'Unauthorized' }
+      })
+
+      cy.get('input[formControlName=email]').type('invaliduser@test.com');
+      cy.get('input[formControlName=password]').type('invalidpassword{enter}{enter}');
+
+      cy.get('.error').should('be.visible');
+
+    });
+
+    it('should navigate to sessions page on successful login', () => {
+      cy.intercept('POST', '/api/auth/login', {
+        body: {
+          id: 1,
+          username: 'userName',
+          firstName: 'firstName',
+          lastName: 'lastName',
+          admin: true
+        },
+      })
+
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '/api/session',
+        },
+        []).as('session')
+
+      cy.get('input[formControlName=email]').type("yoga@studio.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+      cy.url().should('include', '/sessions')
     })
 
-    cy.get('input[formControlName=email]').type('invaliduser@test.com');
-    cy.get('input[formControlName=password]').type('invalidpassword{enter}{enter}');
+    it('should disable submit button if the email has an invalid format', () => {
+      cy.get('input[formControlName=email]').type('invalid-email');
+      cy.get('input[formControlName=password').type('test!1234');
 
-    cy.get('.error').should('be.visible');
+      cy.get('button[type=submit]').should('be.disabled');
+    });
+
+    it('should disable submit button if the password is empty', () => {
+      cy.get('input[formControlName=email]').type('yoga@studio.com');
+
+      cy.get('button[type=submit]').should('be.disabled');
+    });
 
   });
-
-  it('should navigate to sessions page on successful login', () => {
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
-
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
-
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
-    cy.url().should('include', '/sessions')
-  })
-
-  it('should disable submit button if the email has an invalid format', () => {
-    cy.get('input[formControlName=email]').type('invalid-email');
-    cy.get('input[formControlName=password').type('test!1234');
-
-    cy.get('button[type=submit]').should('be.disabled');
-  });
-
-  it('should disable submit button if the password is empty', () => {
-    cy.get('input[formControlName=email]').type('yoga@studio.com');
-
-    cy.get('button[type=submit]').should('be.disabled');
-  });
-
-});
+};
