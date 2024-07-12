1150ac95af90d9db189abb7a43b4338894808333
Keijur0 <tran.qlam@gmail.com>
2024-07-12 18:09:15 +0200

    Back AuthEntryPointJwt unit tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwtTest.java
@@ -0,0 +1,61 @@
+package com.openclassrooms.starterjwt.security.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.AuthenticationException;
+
+@DisplayName("Auth entry point jwt tests")
+public class AuthEntryPointJwtTest {
+    
+        @InjectMocks
+        private AuthEntryPointJwt authEntryPointJwt;
+
+        @Mock
+        private HttpServletRequest request;
+
+        @Mock
+        private HttpServletResponse response;
+
+        @Mock
+        private AuthenticationException authException;
+
+        @Mock
+        private ServletOutputStream outputStream;
+
+        @BeforeEach
+        public void setUp() {
+            MockitoAnnotations.openMocks(this);
+        }
+
+        @DisplayName("Commence method test Unauthorized response")
+        @Test
+        public void testCommence() throws IOException, ServletException {
+            when(response.getOutputStream()).thenReturn(outputStream);
+            when(request.getServletPath()).thenReturn("/path");
+            when(authException.getMessage()).thenReturn("Unauthorized error");
+
+            authEntryPointJwt.commence(request, response, authException);
+
+            verify(response).setContentType("application/json");
+            verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+}
