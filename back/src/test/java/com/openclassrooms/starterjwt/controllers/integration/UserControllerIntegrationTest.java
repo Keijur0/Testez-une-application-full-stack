e6aa45284ffbdf0fbb7c7fb1bbbda72f5d641b7d
Keijur0 <tran.qlam@gmail.com>
2024-07-15 10:15:14 +0200

    Back UserControllerIntegrationTest update

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/integration/UserControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/integration/UserControllerIntegrationTest.java
@@ -1,15 +1,11 @@
 package com.openclassrooms.starterjwt.controllers.integration;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,14 +14,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
 import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
-import com.openclassrooms.starterjwt.services.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @DisplayName("UserController integration tests")
 @SpringBootTest
@@ -41,42 +41,15 @@ public class UserControllerIntegrationTest {
     @Autowired
     private JwtUtils jwtUtils;
 
-    @MockBean
-    private UserService userService;
+    @Autowired 
+    private UserRepository userRepository;
 
     private String token;
 
     @BeforeEach
     public void setUp() {
-        LocalDateTime now = LocalDateTime.now();
-        User user1 = User.builder()
-                .id(1L)
-                .email("john.wick@test.com")
-                .lastName("Wick")
-                .firstName("John")
-                .password("test!1234")
-                .admin(false)
-                .createdAt(now)
-                .updatedAt(now)
-                .build();
-
-        User user2 = User.builder()
-                .id(1L)
-                .email("not.john.wick@test.com")
-                .lastName("Wick")
-                .firstName("NotJohn")
-                .password("test!1234")
-                .admin(false)
-                .createdAt(now)
-                .updatedAt(now)
-                .build();
-
-        Mockito.when(userService.findById(1L)).thenReturn(user1);
-        Mockito.when(userService.findById(2L)).thenReturn(user2);
-        Mockito.when(userService.findById(3L)).thenReturn(null);
-
         Authentication auth = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken("john.wick@test.com", "test!1234")  
+                new UsernamePasswordAuthenticationToken("yoga@studio.com", "test!1234")  
         );
         SecurityContextHolder.getContext().setAuthentication(auth);
         token = jwtUtils.generateJwtToken(auth);
@@ -88,7 +61,8 @@ public class UserControllerIntegrationTest {
         mockMvc.perform(get("/api/user/1")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value("1"));
     }
 
     @DisplayName("Find user by id test with non-existing user")
@@ -111,7 +85,28 @@ public class UserControllerIntegrationTest {
     @DisplayName("Delete user by id with existing and authorized user")
     @Test
     public void testDelete_UserExistsAndAuthorized() throws Exception {
-        mockMvc.perform(delete("/api/user/1")
+
+        // Creating a new user to authenticate with and to delete
+        ObjectMapper mapper = new ObjectMapper();
+        SignupRequest register = new SignupRequest();
+        register.setEmail("testuser@test.com");
+        register.setFirstName("test");
+        register.setLastName("test");
+        register.setPassword("test!1234");
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(register)));
+
+        User user = userRepository.findByEmail("testuser@test.com").orElseThrow(null);
+
+        Authentication auth = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken("testuser@test.com", "test!1234")  
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        token = jwtUtils.generateJwtToken(auth);
+
+        mockMvc.perform(delete("/api/user/" + user.getId())
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk());
     }
@@ -119,16 +114,28 @@ public class UserControllerIntegrationTest {
     @DisplayName("Delete user by id with existing but unauthorized user")
     @Test
     public void testDelete_UserExistsButNotAuthorized() throws Exception {
-        mockMvc.perform(delete("/api/user/2")
+        User user = User.builder()
+                .email("testuser@test.com")
+                .lastName("test")
+                .firstName("test")
+                .password("test!1234")
+                .admin(false)
+                .build();
+
+        userRepository.save(user);
+
+        mockMvc.perform(delete("/api/user/" + user.getId())
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isUnauthorized());
+        
+        userRepository.deleteById(user.getId());
 
     }
 
     @DisplayName("Delete user by id with non-existing user")
     @Test
     public void testDelete_UserDoesNotExist() throws Exception {
-        mockMvc.perform(delete("/api/user/3")
+        mockMvc.perform(delete("/api/user/999999")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isNotFound());
     }
